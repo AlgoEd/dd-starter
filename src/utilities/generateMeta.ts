53efa2533eacad7d0d750e40f1a24ec5d22b0daa
Xiaojian Wang <xiaojian@algoed.co>
2026-04-15 19:23:53 +0800

    fix: don't prepend server URL to absolute OG image URLs
    Images on R2/CDN already have full URLs. The old code always prepended
    the server URL, producing broken double-URL like
    https://pages.algoed.co/https://cdn.example.com/image.png

--- src/utilities/generateMeta.ts
+++ src/utilities/generateMeta.ts
@@ -11,9 +11,8 @@ const getImageURL = (image?: Media | Config['db']['defaultIDType'] | null) => {
   let url = ''
 
   if (image && typeof image === 'object' && 'url' in image) {
-    const ogUrl = image.sizes?.og?.url
-
-    url = ogUrl ? serverUrl + ogUrl : serverUrl + image.url
+    const ogUrl = image.sizes?.og?.url || image.url || ''
+    url = ogUrl.startsWith('http') ? ogUrl : serverUrl + ogUrl
   }
 
   return url
